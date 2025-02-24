3568bc2158e449b1af7fd239dee1cfa08910af90
Timonwa Akintokun <timonwaakintokun@gmail.com>
2025-02-24 23:10:31 +0100

    feat: add Hero component and HomeContent wrapper, update Home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-// import Image from "next/image";
-// vscode:extension/bradlc.vscode-tailwindcss
+import HomeContent from "@/components/home";
+
 export default function Home() {
-  return (
-    <div className="pb-20sm:p-20 grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 font-Groteskbold">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <h1 className="text-4xl font-medium leading-tight tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
-          Book Scientific and Diagnostic Laboratory Services
-        </h1>
-      </main>
-    </div>
-  );
+  return <HomeContent />;
 }
---
+++ src/components/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./Hero";
+
+const HomeContent = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default HomeContent;
