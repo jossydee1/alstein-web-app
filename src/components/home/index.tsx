a49cb701fbe9b3779e717c5fe2f096858b9a7a2e
Timonwa Akintokun <timonwaakintokun@gmail.com>
2025-02-26 12:07:14 +0100

    feat: add Services component and integrate it into HomeContent; update types and services data

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "./Hero";
 import WhyUs from "./WhyUs";
 import Categories from "./Categories";
-import Listings from "./Listings";
+import Services from "./Services";
 import Testimonials from "./Testimonials";
 import Partners from "./Partners";
 
@@ -12,7 +12,7 @@ const HomeContent = () => {
       <Hero />
       <WhyUs />
       <Categories />
-      <Listings />
+      <Services />
       <Testimonials />
       <Partners />
     </main>
