9d65f4a07f2d6081e348c3b5df3e003c02739090
Timonwa Akintokun <timonwaakintokun@gmail.com>
2025-02-28 17:15:36 +0100

    feat: update Reviews component layout and enhance feedback form

--- src/components/listing-details/Reviews.tsx
+++ src/components/listing-details/Reviews.tsx
@@ -7,7 +7,7 @@ import { Button } from "../ui/button";
 const Reviews = () => {
   return (
     <section>
-      <hr className="my-6 border border-[#EBEBEB]" />
+      <hr className="my-6border border-[#EBEBEB]" />
 
       <div>
         <h2 className="font-500 mb-3 text-center text-2xl">Likes & Reviews</h2>
@@ -27,8 +27,8 @@ const Reviews = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 gap-16 lg:grid-cols-2">
-        <div className="grid max-w-[580px] gap-16">
+      <div className="flex flex-col items-start justify-between gap-6 md:flex-row">
+        <div className="grid w-full max-w-[580px] gap-16">
           {[1, 1].map((_, i) => (
             <div key={i}>
               <div className="flex items-center gap-3">
@@ -60,19 +60,39 @@ const Reviews = () => {
           </Button>
         </div>
 
-        <div className="gap-4 border border-[#E6E7EA] p-4">
+        <div className="w-full max-w-[480px] gap-4 rounded-md border border-[#E6E7EA] p-4">
           <h3 className="text-xl font-semibold text-[#010814]">
             Share your feedback
           </h3>
 
           <form>
-            <label className="mb-16 text-sm text-[#354259]">
-              How was you experience?
-            </label>
-            <textarea
-              className="mt-1 block h-[120px] w-full rounded-md border border-[#E6E7EA] p-3"
-              placeholder="Write your review here..."
-            ></textarea>
+            <div className="mb-4">
+              <label className="mb-16 text-sm text-[#354259]">
+                How was you experience?
+              </label>
+            </div>
+
+            <div>
+              <label className="text-sm text-[#354259]">
+                Can you tell us more?
+              </label>
+              <textarea className="block h-[160px] w-full rounded-md border border-[#E6E7EA] p-3"></textarea>
+            </div>
+
+            <div className="mt-4 flex gap-4">
+              <Button
+                variant="outline"
+                className="flex-1 px-6 py-2.5 font-medium text-[#031330]"
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="outline"
+                className="flex-1 bg-[#2D84F1] px-6 py-2.5 font-medium text-white"
+              >
+                Submit
+              </Button>
+            </div>
           </form>
         </div>
       </div>
