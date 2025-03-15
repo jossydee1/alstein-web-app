a6a514de975dc9dc0eec67bbb23c5cbd8b5ff69e
timonwa <timonwaakintokun@gmail.com>
2025-03-15 22:42:40 +0100

    feat: redirect authenticated users to dashboard on Auth layout load

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,21 @@
+import { useAuth } from "@/context";
+import { dashboardRoutes } from "@/utils";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const { userId, token } = useAuth();
+
+  useEffect(() => {
+    if (userId && token) {
+      router.push(dashboardRoutes.overview);
+    }
+  }, [token, userId, router]);
+
   return <div>{children}</div>;
 }
