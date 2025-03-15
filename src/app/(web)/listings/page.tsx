e02df3b4c8934f5a7b2175a081e4168d22a3f792
timonwa <timonwaakintokun@gmail.com>
2025-03-15 23:10:26 +0100

    feat: integrate categories into ListingsContent and FilterMenu components

--- src/app/(web)/listings/page.tsx
+++ src/app/(web)/listings/page.tsx
@@ -1,8 +1,13 @@
 import ListingsContent from "@/components/web/listings";
+import { serverFetch } from "@/utils";
 import React from "react";
 
+const categories = await serverFetch(
+  "/client/public/api/v1/equipments/get-equipment-category?skip=0&take=3",
+);
+
 const Page = () => {
-  return <ListingsContent />;
+  return <ListingsContent categories={categories} />;
 };
 
 export default Page;
