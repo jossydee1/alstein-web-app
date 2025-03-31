998fca758ef602a8801ed8e4e49b751e8aa09117
timonwa <timonwaakintokun@gmail.com>
2025-03-31 04:47:42 +0100

    feat: implement confirmation page with transaction verification and error handling

--- src/app/(web)/confirmation/page.tsx
+++ src/app/(web)/confirmation/page.tsx
@@ -1,15 +1,8 @@
+import ConfirmationPageContent from "@/components/web/confirmation";
 import React from "react";
 
 const Confirmation = () => {
-  return (
-    <main className="relative mb-16">
-      <div className="section-container !pb-9 !pt-0">
-        <div className="mt-8 grid grid-cols-1 gap-10 md:grid-cols-2">
-          <p>Payment was successfull!</p>
-        </div>
-      </div>
-    </main>
-  );
+  return <ConfirmationPageContent />;
 };
 
 export default Confirmation;
--- src/utils/others/constants.ts
+++ src/utils/others/constants.ts
@@ -2,3 +2,5 @@ export const API_URL =
   "https://gg1l8ijqo1.execute-api.us-east-1.amazonaws.com/dev/";
 export const PAYSTACK_PUBLIC_TEST_KEY =
   "pk_test_b14df591038d0b42d030b17d377d8cb5fabce945";
+export const PAYSTACK_TEST_KEY =
+  "pk_test_b14df591038d0b42d030b17d377d8cb5fabce945";
