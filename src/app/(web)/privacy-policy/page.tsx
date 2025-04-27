71d1c62e7802b0fd764aa04597704714e96317cd
timonwa <timonwaakintokun@gmail.com>
2025-04-27 09:54:07 +0100

    feat: add Privacy Policy and Terms and Conditions pages with styling

---
+++ src/app/(web)/privacy-policy/page.tsx
@@ -0,0 +1,8 @@
+import PrivacyPageContent from "@/components/web/legalities/privacy";
+import React from "react";
+
+const Page = () => {
+  return <PrivacyPageContent />;
+};
+
+export default Page;
---
+++ src/app/(web)/terms/page.tsx
@@ -0,0 +1,8 @@
+import TermsPageContent from "@/components/web/legalities/terms";
+import React from "react";
+
+const Page = () => {
+  return <TermsPageContent />;
+};
+
+export default Page;
