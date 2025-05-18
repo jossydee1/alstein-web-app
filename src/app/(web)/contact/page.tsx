4ad5196bbbb0d6d21fe379a4bb7d22884308ebdc
timonwa <timonwaakintokun@gmail.com>
2025-05-18 15:51:48 +0100

    feat: add metadata for various pages to improve SEO and user experience

--- src/app/(web)/contact/page.tsx
+++ src/app/(web)/contact/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Alstein | Contact Us",
+  description:
+    "Simplifying access to medical diagnostics and research tools. Find Laboratories, and Research Equipment Near You.",
+};
+
 export default async function Page() {
   return <div>Contact Page</div>;
 }
